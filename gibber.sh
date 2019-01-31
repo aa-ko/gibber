362fda77907f4691a88102be9b2c87a3830252d4
Aaron Korte <aaron.korte@bordenau.de>
2019-01-31 10:56:03 +0100

    script now skips files with conversion fails

--- gibber.sh
+++ gibber.sh
@@ -10,12 +10,21 @@ function convertencoding {
     elif [ "$ORG_ENC" == "$NEW_ENC" ]; then
         echo "Skipping. source encoding matches target encoding: $FILE_NAME"
     else
-        echo "$ORG_ENC -> $NEW_ENC : $FILE_NAME"
-        iconv -s -f $ORG_ENC -t $NEW_ENC $FILE_NAME 2>> reencode.log 1> .utf-8.tmp
-        cat .utf-8.tmp > $FILE_NAME
+        iconv -s -f $ORG_ENC -t $NEW_ENC $FILE_NAME 2>> gibber.log 1> .utf-8.tmp
+        NEW_BYTES=$(du -b .utf-8.tmp | sed -r 's/([0-9]+).*/\1/')
+        if [ $NEW_BYTES -eq 0 ]; then
+            echo "Conversion failed! $FILE_NAME"
+            echo "$FILE_NAME" >> gibber_errlist.log
+        else
+            echo "$ORG_ENC -> $NEW_ENC : $FILE_NAME"
+            cat .utf-8.tmp > $FILE_NAME
+        fi
     fi
 }
 export -f convertencoding
 
 echo "Converting all files matching the filter '$2' in path '$1' to specified encoding '$NEW_ENC'."
+echo ""
 find $1 -type f -iname "$2" -exec bash -c 'convertencoding "$0"' {} \;
+
+rm .utf-8.tmp
